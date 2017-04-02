85988529da304c9b007d2deb672e53d756280064
codegrabber1 <poruchenko@ukr.net>
2017-04-02 03:14:14 +0300

    0204_0314

---
+++ app/js/myscripts.js
@@ -0,0 +1,27 @@
+// top menu
+
+  $(".sf-menu").superfish();
+  $(".sf-menu").after("<div id='my-menu'>");
+  $(".sf-menu").clone().appendTo("#my-menu");
+  $("#my-menu").find("*").attr('style', '');
+  $("#my-menu").find("ul").removeClass("sf-menu");
+  $("#my-menu").mmenu({
+    extensions : [ 'widescreen', 'theme-dark', 'pageshadow', 'effect-menu-slide', 'effect-listitems-slide' ],
+    navbar: {
+      title: "Anton Krasov"
+    }
+  });
+  var api = $("#my-menu").data("mmenu");
+  api.bind("closed", function(){
+    $(".toggle-mnu").removeClass("on");
+  });
+
+  $(".mobile-mnu").click(function(){
+   var mmAPI = $("#my-menu").data("mmenu");
+   mmAPI.open();
+   var thiss = $(this).find(".toggle-mnu");
+   thiss.toggleClass("on");
+   $(".main-mnu").slideToggle();
+        return false;
+   });
+// end top menu
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
-
-const gulp  = require('gulp');
-const sass  = require('gulp-sass');
-const pug   = require('gulp-pug');
-const notify    = require('gulp-notify');
-const bs = require('browser-sync').create();
-const autopref  = require('gulp-autoprefixer');
+const gulp  	   = require('gulp');
+const sass  	   = require('gulp-sass');
+const pug   	   = require('gulp-pug');
+const multipipe  = require('multipipe');
+const notify     = require('gulp-notify');
+const concatcss  = require('gulp-concat-css');
+const concat     = require('gulp-concat');
+const bs 		     = require('browser-sync').create();
+const autopref   = require('gulp-autoprefixer');
 
 // Static server
 gulp.task('server', function () {
@@ -16,13 +18,27 @@ gulp.task('server', function () {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('app/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError)),
-    // .pipe(autopref({
-    //   browsers: ['last 2 versions'],
-    //   cascade: false
-    // })),
-    .pipe(gulp.dest('style.css'));
+  return multipipe(
+      gulp.src('app/sass/**/*.sass'),
+      sass().on('error', sass.logError),
+      concatcss('style.css'),
+      gulp.dest('app/')
+  ).on('error', notify.onError());
+});
+
+/*=== jade(pug) ===*/
+gulp.task('js', function(){
+  return multipipe(
+    gulp.src([
+      'app/libs/jquery/jquery.js',
+      'app/libs/respond/respond.min.js',
+      'app/libs/jq.mmnu/js/jquery.mmenu.all.min.js',
+      'app/libs/superfish/dist/js/superfish.js',
+      'app/libs/waypoints/waypoints.min.js',
+    ]),
+    concat("libs.js"),
+    gulp.dest('app/js/')
+  ).on('error', notify.onError());
 });
 
 /*=== jade(pug) ===*/
